34a3581fb986c06dd2c56fc773330f34fa0c30b1
mahir-hs <mahirhasan333@gmail.com>
2024-12-22 13:57:28 +0600

    DP-1D

---
+++ 198.house-robber.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=198 lang=cpp
+ *
+ * [198] House Robber
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+        if (n == 2)
+            return max(nums[0], nums[1]);
+        if (n == 3)
+            return max(nums[0] + nums[2], nums[1]);
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[0] + nums[2];
+        for (int i = 3; i < n; i++)
+        {
+            dp[i] = max(dp[i - 1], max(dp[i - 2], dp[i - 3]) + nums[i]);
+        }
+        return dp[n - 1];
+    }
+};
+// @lc code=end
