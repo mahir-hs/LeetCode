29d6a27d4d6d05b9279bb0d65d2c26f22844441d
mahir-hs <mahirhasan333@gmail.com>
2025-08-30 22:15:44 +0600

    implement plusOne function to increment a number represented as an array of digits

---
+++ 66.plus-one.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=66 lang=golang
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+func plusOne(digits []int) []int {
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i] = digits[i] + 1
+			return digits
+		}
+		digits[i] = 0
+	}
+	return append([]int{1}, digits...)
+}
+
+// @lc code=end
+
